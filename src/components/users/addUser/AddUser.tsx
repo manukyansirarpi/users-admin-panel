a132c8a70483ed0096dea2af64624e6eae67c351
Manukyan Sirarpi <manukyansirarpi@gmail.com>
2022-12-03 14:21:59 +0400

    validate form

--- src/components/users/addUser/AddUser.tsx
+++ src/components/users/addUser/AddUser.tsx
@@ -11,6 +11,36 @@ import { UserI } from '../api/UsersI';
 import Input from '../../../ui/Input';
 import classes from './AddUser.module.css';
 
+type Validatable = {
+    value: string;
+    error: boolean;
+    errorMessage: string;
+}
+
+interface UserFormDataI extends Omit<UserI, 'name' | 'email'> {
+  name: Validatable, 
+  email:Validatable,
+}
+
+const userInitialState: UserFormDataI = {
+  id: 0,
+  name: {
+   value:'',
+   error:false,
+   errorMessage:'Invalid name'
+ }, 
+  email: {
+   value:'',
+   error:false,
+   errorMessage:'Invalid email'
+ },
+  photo: "",
+  location: "",
+  registeredDate: "",
+  lastActiveDate: "",
+  disabled: false
+};
+
 const AddUser: React.FC = () => {
 
     const dispatch = useAppDispatch();
@@ -22,23 +52,12 @@ const AddUser: React.FC = () => {
 
     const [photo, setPhoto] = useState<File>();
     const [photoLoading, setPphotoLoading] = useState<boolean>(false);
-    const [userData, setUserData] = useReducer(
-      (state: UserI, newState: {}) => ({...state, ...newState}) ,
-      {  id: 0,
-         name: "", 
-         email: "",
-         photo: "",
-         location: "",
-         registeredDate: "",
-         lastActiveDate: "",
-         disabled: false
-       }
-     );
+    const [userData, setUserData] = useReducer((state: UserFormDataI, newState: {}) => ({...state, ...newState}), userInitialState );
     
     useEffect(()=>{
       if(userId) {
         getUserData(parseInt(userId)).then((user) => {
-          setUserData(user);
+          setUserData({...user, name: {value: user.name }, email: {value: user.email}});
         });
       }
     }, [userId]);
@@ -55,16 +74,6 @@ const AddUser: React.FC = () => {
       }
     }, [photo, dispatch]);
 
-    const cancelHandler = () => {
-      navigate(location.state?.urlParams? `/${location.state?.urlParams}` : '/');
-    }
-  
-    const handleInput = (e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
-      const name = e.target.name;
-      const newValue = e.target.value;
-      setUserData({ [name]: newValue });
-    };
-
     const onPhotoInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
       if(e.target.files && e.target.files[0]){
         setPhoto(e.target.files[0]);
@@ -76,14 +85,56 @@ const AddUser: React.FC = () => {
       element.value = ''
     }
 
+    const cancelHandler = () => {
+      navigate(location.state?.urlParams? `/${location.state?.urlParams}` : '/');
+    }
+
+    const isFormValid = () => {
+      if (!userData.name.value || !userData.email.value) {
+        if (!userData.name.value) {
+          setUserData({'name': {...userData.name, ...{error: true}} });
+        }  
+        if (!userData.email.value) {
+          setUserData({ 'email': {...userData.email, ...{error: true}} });
+        }  
+        return false;
+      }
+      return true;
+    }
+  
+    const handleInput = (e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
+      const name = e.target.name;
+      const newValue = e.target.value;
+      if(name === 'name') {
+        if(!newValue.trim().length) {
+          setUserData({'name': {...userData.name, value: newValue, error: true} });
+        } else {
+          setUserData({'name': {...userData.name, value: newValue, error: false} });
+        }
+      } else if(name === 'email') {
+        if(!newValue.trim().length) {
+          setUserData({'email': {...userData.email,value: newValue, error: true} });
+        } else {
+          setUserData({'email': {...userData.email, value: newValue, error: false} });
+        }
+      } else {
+        setUserData({ [name]: newValue });
+      }
+    };
+
     const handleSubmit = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
       e.preventDefault();
-      if(userId) {
-        dispatch(updateUserAsync(userData));
-      } else {
-        dispatch(addUserAsync(userData));
+      if (!isFormValid()) {
+        return;
+      }  else {
+        const newData =  {...userData,  name: userData.name.value, email: userData.email.value};
+        if(userId) {
+          dispatch(updateUserAsync(newData));
+        } else {
+          dispatch(addUserAsync(newData));
+        }
+        cancelHandler();
       }
-      cancelHandler();
     };
 
     return (
@@ -91,7 +142,7 @@ const AddUser: React.FC = () => {
         <Container maxWidth="sm">
           <form>
             <FormGroup>
-              <Input name="name" value={userData.name}  placeholder="User Name" onChange={handleInput}></Input>
+              <Input name="name" value={userData.name.value} error={userData.name.error} helperText={userData.name.error && userData.name.errorMessage} placeholder="User Name" onChange={handleInput}></Input>
               <FormControl>
                   {photoLoading && <LinearProgress className={classes.avatar}  />}
                   {userData && userData.photo && !photoLoading && <img className={classes.avatar} alt='User Avatar' src ={userData.photo} />}
@@ -99,7 +150,7 @@ const AddUser: React.FC = () => {
                   <input hidden type="file" onClick={onPhotoInputClick} onChange={onPhotoInputChange} accept="image/png, image/gif, image/jpeg"/>
                 </Button>
               </FormControl>
-              <Input name="email" value={userData.email}  placeholder="Email" onChange={handleInput}></Input>
+              <Input name="email" value={userData.email.value} error={userData.email.error}  helperText={userData.email.error && userData.email.errorMessage} placeholder="Email" onChange={handleInput}></Input>
               <Input name="location" value={userData.location}  placeholder="Location" onChange={handleInput}></Input>
             </FormGroup>
             <Stack direction="row" justifyContent="flex-start">
